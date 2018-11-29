63cf8893ec5211e0b873d187da8646179f71fbd1
Thomas <thomasan1995@gmail.com>
2018-11-28 20:59:41 -0800

    testing pruning

---
+++ compression/prune_utils.py
@@ -0,0 +1,11 @@
+def print_model_parameters(model, with_values=False):
+    print(f"{'Param name':20} {'Shape':30} {'Type':15}")
+    print('-'*70)
+    for name, param in model.named_parameters():
+        print(f'{name:20} {str(param.shape):30} {str(param.dtype):15}')
+        if with_values:
+            print(param)
+def log(filename, content):
+    with open(filename, 'a') as f:
+        content += "\n"
+        f.write(content)
