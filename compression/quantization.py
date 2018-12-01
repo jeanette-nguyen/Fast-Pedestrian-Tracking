01c12d72bcafd3ce570b62cf61955e78d01b0d62
Thomas <thomasan1995@gmail.com>
2018-12-01 15:42:13 -0800

    added filtering for convolutional layers

--- compression/quantization.py
+++ compression/quantization.py
@@ -13,16 +13,32 @@ def quantize(model, bits=5):
     for module in model.children():
         dev = module.weight.device
         weight = module.weight.data.cpu().numpy()
-        (r, c) = weight.shape
-        if r < c:
-            matrix = csr_matrix(weight)
+        shape = weight.shape
+        if len(shape) == 2:
+            if r < c:
+                matrix = csr_matrix(weight)
+            else:
+                matrix = csc_matrix(weight)
+            mmin = min(matrix.data)
+            mmax = max(matrix.data)
+            space = np.linspace(mmin, mmax, num=2**bits)
+            kmeans = KMeans(n_clusters=len(space), init=space.reshape(-1, 1), n_init=1, precompute_distances=True, algorithm="full")
+            kmeans.fit(matrix.data.reshape(-1, 1))
+            new_weights = kmeans.cluster_centers_[kmeans.labels_].reshape(-1)
+            matrix.data = new_weights
+            module.weight.data = torch.from_numpy(matrix.toarray()).to(dev)
         else:
-            matrix = csc_matrix(weight)
-        mmin = min(matrix.data)
-        mmax = max(matrix.data)
-        space = np.linspace(mmin, mmax, num=2**bits)
-        kmeans = KMeans(n_clusters=len(space), init=space.reshape(-1, 1), n_init=1, precompute_distances=True, algorithm="full")
-        kmeans.fit(matrix.data.reshape(-1, 1))
-        new_weights = kmeans.cluster_centers_[kmeans.labels_].reshape(-1)
-        matrix.data = new_weights
-        module.weight.data = torch.from_numpy(matrix.toarray()).to(dev)
+            assert len(shape) == 4, "must be 2 or 4 for FC layers or convolution filters"
+            for in_c in range(shape[0]):
+                for out_c in range(shape[1]):
+                    k = weight[in_c, out_c, :, :]
+                    shape_k = k.shape
+                    matrix = csr_matrix(weight[in_c, out_c, :, :]) if shape_k[0] < shape_k[1] else csc_matrix(weight)
+                    mmin = min(matrix.data)
+                    mmax = max(matrix.data)
+                    space = np.linspace(mmin, mmax, num=2**bits)
+                    kmeans = KMeans(n_clusters=len(space), init=space.reshape(-1, 1), n_init=1, precompute_distances=True, algorithm="full")
+                    kmeans.fit(matrix.data.reshape(-1, 1))
+                    new_weights = kmeans.cluster_centers_[kmeans.labels_].reshape(-1)
+                    matrix.data = new_weights
+                    module.weight[in_c, out_c].data = torch.from_numpy(matrix.toarray()).to(dev)
