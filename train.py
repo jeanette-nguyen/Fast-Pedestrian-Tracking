b6ec29b21ef17db8e2863c5ccb976e7f0a38ceef
Kevin Le <ktl014@ucsd.edu>
2018-12-14 00:26:24 -0800

    keep working model to train

--- train.py
+++ train.py
@@ -2,6 +2,8 @@ from __future__ import  absolute_import
 # though cupy is not used but without this line, it raise errors...
 import cupy as cp
 import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "8"
 
 import ipdb
 import matplotlib.pyplot as plt
@@ -13,7 +15,7 @@ from model.faster_rcnn_vgg16 import FasterRCNNVGG16
 from torch.utils import data as data_
 from trainer import FasterRCNNTrainer
 from utils import array_tool as at
-from utils.vis_tool import vis_bbox
+from utils.vis_tool import visdom_bbox
 from utils.eval_tool import eval_detection_voc
 
 # fix for ulimit
@@ -21,7 +23,7 @@ from utils.eval_tool import eval_detection_voc
 import resource
 
 rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
-resource.setrlimit(resource.RLIMIT_NOFILE, (20480, rlimit[1]))
+resource.setrlimit(resource.RLIMIT_NOFILE, (2048, rlimit[1]))
 
 def transform_bbox(bbox_):
     """
@@ -75,37 +77,60 @@ def train(opt, faster_rcnn, dataloader, test_dataloader, trainer, lr_, best_map)
                 roicls = losses[3].cpu().data.numpy()
                 tot = losses[4].cpu().data.numpy()
                 pbar.set_description(f"Epoch: {epoch} | Batch: {ii} | RPNLoc Loss: {rpnloc:.4f} | RPNclc Loss: {rpncls:.4f} | ROIloc Loss: {roiloc:.4f} | ROIclc Loss: {roicls:.4f} | Total Loss: {tot:.4f}")
-            if (ii + 1) % 1000 == 0:
+            if (ii + 1) % 100 == 0:
+                # plot loss
+                trainer.vis.plot_many(trainer.get_meter_data())
                 print(trainer.get_meter_data())
                 try:
                     ori_img_ = inverse_normalize(at.tonumpy(img[0]))
-                    gt_img = vis_bbox(ori_img_,
+                    gt_img = visdom_bbox(ori_img_,
                                         at.tonumpy(bbox_[0]),
                                         at.tonumpy(label_[0]))
+                    trainer.vis.img('gt_img', gt_img)
                     plt.show()
 
                     # plot predicti bboxes
                     _bboxes, _labels, _scores = trainer.faster_rcnn.predict([ori_img_], visualize=True)
-                    pred_img = vis_bbox(ori_img_,
+                    pred_img = visdom_bbox(ori_img_,
                                         at.tonumpy(_bboxes[0]),
                                         at.tonumpy(_labels[0]).reshape(-1),
                                         at.tonumpy(_scores[0]))
                     plt.show()
+                    trainer.vis.img('pred_img', pred_img)
+
+                    # rpn confusion matrix(meter)
+                    trainer.vis.text(str(trainer.rpn_cm.value().tolist()),
+                                     win='rpn_cm')
+                    # roi confusion matrix
+                    trainer.vis.img('roi_cm', at.totensor(trainer.roi_cm.conf,
+                                                          False).float())
                 except:
                     print("Cannot display images")
-                
+            if (ii + 1) % 500 == 0:
+                eval_result = eval(test_dataloader, faster_rcnn, test_num=250)
+                trainer.vis.plot('val_map', eval_result['map'])
+                log_info = 'lr:{}, map:{},loss:{}'.format(str(lr_), str(
+                    eval_result['map']), str(trainer.get_meter_data()))
+                trainer.vis.log(log_info)
+
+
+        # Save after every epoch
+        epoch_path = trainer.save(epoch, best_map=0, model_name=opt.model_name)
                 
         eval_result = eval(test_dataloader, faster_rcnn, test_num=1000)
+        trainer.vis.plot('test_map', eval_result['map'])
         lr_ = trainer.faster_rcnn.optimizer.param_groups[0]['lr']
         log_info = 'lr:{}, map:{},loss:{}'.format(str(lr_), str(eval_result['map']),
                                                         str(trainer.get_meter_data()))
+        trainer.vis.log(log_info)
         print("Evaluation Results: ")
         print(log_info)
         print("\n\n")
         
         if eval_result['map'] > best_map:
             best_map = eval_result['map']
-            best_path = trainer.save(best_map=best_map)
+            best_path = epoch_path
+
         if epoch == 9:
             trainer.load(best_path)
             trainer.faster_rcnn.scale_lr(opt.lr_decay)
